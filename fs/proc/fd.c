f810f5379da90359eff7c0a60853996be402cbe6
Oleg Nesterov <oleg@redhat.com>
2013-09-11 14:24:37 -0700

    UPSTREAM: proc: make proc_fd_permission() thread-friendly
    (cherry pick from commit 96d0df79f2644fc823f26c06491e182d87a90c2a)
    
    proc_fd_permission() says "process can still access /proc/self/fd after it
    has executed a setuid()", but the "task_pid() = proc_pid() check only
    helps if the task is group leader, /proc/self points to
    /proc/<leader-pid>.
    
    Change this check to use task_tgid() so that the whole thread group can
    access its /proc/self/fd or /proc/<tid-of-sub-thread>/fd.
    
    Notes:
    	- CLONE_THREAD does not require CLONE_FILES so task->files
    	  can differ, but I don't think this can lead to any security
    	  problem. And this matches same_thread_group() in
    	  __ptrace_may_access().
    
    	- /proc/self should probably point to /proc/<thread-tid>, but
    	  it is too late to change the rules. Perhaps it makes sense
    	  to add /proc/thread though.
    
    Test-case:
    
    	void *tfunc(void *arg)
    	{
    		assert(opendir("/proc/self/fd"));
    		return NULL;
    	}
    
    	int main(void)
    	{
    		pthread_t t;
    		pthread_create(&t, NULL, tfunc, NULL);
    		pthread_join(t, NULL);
    		return 0;
    	}
    
    fails if, say, this executable is not readable and suid_dumpable = 0.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Bug: 26016905
    Change-Id: Ifdd6403a8fccd073122e89d3547c13ccc08f0dce
    Signed-off-by: Joe Maples <joe@frap129.org>
    
    Conflicts:
    	fs/proc/fd.c

--- fs/proc/fd.c
+++ fs/proc/fd.c
@@ -308,11 +308,8 @@ int proc_fd_permission(struct inode *inode, int mask)
 
 	rv = generic_permission(inode, mask);
 	if (rv == 0)
-		return rv;
-
-	rcu_read_lock();
-	p = pid_task(proc_pid(inode), PIDTYPE_PID);
-	if (p && same_thread_group(p, current))
+		return 0;
+	if (task_tgid(current) == proc_pid(inode))
 		rv = 0;
 	rcu_read_unlock();
 
