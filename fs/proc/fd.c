df72b5f6407845ed9f83371fde19c7ae6e434513
Oleg Nesterov <oleg@redhat.com>
2015-11-06 16:30:06 -0800

    UPSTREAM: proc: actually make proc_fd_permission() thread-friendly
    (cherry pick from commit 54708d2858e79a2bdda10bf8a20c80eb96c20613)
    
    The commit 96d0df79f264 ("proc: make proc_fd_permission() thread-friendly")
    fixed the access to /proc/self/fd from sub-threads, but introduced another
    problem: a sub-thread can't access /proc/<tid>/fd/ or /proc/thread-self/fd
    if generic_permission() fails.
    
    Change proc_fd_permission() to check same_thread_group(pid_task(), current).
    
    Fixes: 96d0df79f264 ("proc: make proc_fd_permission() thread-friendly")
    Reported-by: "Jin, Yihua" <yihua.jin@intel.com>
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Cc: "Eric W. Biederman" <ebiederm@xmission.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Bug: 26016905
    Change-Id: I1894c78d0b13f0bde8cde84bd142ba67590dc0f1

--- fs/proc/fd.c
+++ fs/proc/fd.c
@@ -308,8 +308,11 @@ int proc_fd_permission(struct inode *inode, int mask)
 
 	rv = generic_permission(inode, mask);
 	if (rv == 0)
-		return 0;
-	if (task_tgid(current) == proc_pid(inode))
+		return rv;
+
+	rcu_read_lock();
+	p = pid_task(proc_pid(inode), PIDTYPE_PID);
+	if (p && same_thread_group(p, current))
 		rv = 0;
 	rcu_read_unlock();
 
