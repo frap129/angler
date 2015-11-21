e7ec76baed3cae90bc58cd018a2acfd3c7dfdd2e
Shoaib0597 <Shoaib0595@gmail.com>
2015-11-21 18:49:04 +0530

    Fixed High-Load Average from UnInterruptible Waits
    Conflicts:
    	drivers/video/msm/mdss/mdss_fb.c
    
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -3105,10 +3105,10 @@ static int __mdss_fb_display_thread(void *data)
 				(atomic_read(&mfd->commits_pending) ||
 				 kthread_should_stop()));
 
-		if (ret) {
-			pr_info("%s: interrupted", __func__);
-			continue;
-		}
+		      if (ret) {
+ 				pr_info("%s: interrupted", __func__);
+ 				continue;
+ 		      }
 
 		if (kthread_should_stop())
 			break;
