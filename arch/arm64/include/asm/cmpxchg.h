abf7ae7ddf63493845f7e2742dff05549f517848
Joe Maples <joe@frap129.org>
2016-08-20 00:27:21 +0000

    Revert "LLVMLinux: Revert cmpxchg-warning-aarch64.patch to fix warnings"
    This reverts commit 07032e7c04c1898d287d2918b5fa7b1d63a7efbf.

--- arch/arm64/include/asm/cmpxchg.h
+++ arch/arm64/include/asm/cmpxchg.h
@@ -65,6 +65,7 @@ static inline unsigned long __xchg(unsigned long x, volatile void *ptr, int size
 		break;
 	default:
 		BUILD_BUG();
+		__compiletime_error();
 	}
 
 	smp_mb();
@@ -147,6 +148,7 @@ static inline unsigned long __cmpxchg(volatile void *ptr, unsigned long old,
 
 	default:
 		BUILD_BUG();
+		__compiletime_error();
 	}
 
 	return oldval;
