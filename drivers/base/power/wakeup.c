6ffc4cc50fc9b8901b3c242a38fab2e09ea6e576
Joe Maples <joe@frap129.org>
2016-08-28 03:30:10 +0000

    Revert "wakeup: add toggles for wlan wakelocks. They are all enabled by default, it's up to the user and I provide no support if Wi-Fi stops working normally without these locks enabled. This is for advanced users"
    This reverts commit e95ad0e24840bc25dc8f0bc1436fc16d226548a8.

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -28,12 +28,6 @@ static bool enable_wlan_wake_ws = true;
 module_param(enable_wlan_wake_ws, bool, 0644);
 static bool enable_bluedroid_timer_ws = true;
 module_param(enable_bluedroid_timer_ws, bool, 0644);
-static bool enable_wlan_rx_wake_ws = true;
-module_param(enable_wlan_rx_wake_ws, bool, 0644);
-static bool enable_wlan_ctrl_wake_ws = true;
-module_param(enable_wlan_ctrl_wake_ws, bool, 0644);
-static bool enable_wlan_wake_ws = true;
-module_param(enable_wlan_wake_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -510,15 +504,6 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 		wakeup_source_deactivate(ws);
 		return;
 	}
-
-	if (!enable_wlan_rx_wake_ws && !strcmp(ws->name, "wlan_rx_wake"))
-                return;
-
-	if (!enable_wlan_ctrl_wake_ws && !strcmp(ws->name, "wlan_ctrl_wake"))
-                return;
-
-	if (!enable_wlan_wake_ws && !strcmp(ws->name, "wlan_wake"))
-                return;
 
 	/*
 	 * active wakeup source should bring the system
