fdb381105e08af3290c2518ba9669e474d1fb371
Joe Maples <joe@frap129.org>
2016-11-03 20:07:30 +0000

    Process Reclaim fix
    Signed-off-by: Joe Maples <joe@frap129.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1197,14 +1197,15 @@ unsigned long reclaim_pages_from_list(struct list_head *page_list,
 
 	unsigned long nr_reclaimed;
 	struct page *page;
-	unsigned long dummy1, dummy2;
+	unsigned long dummy1, dummy2, dummy3, dummy4, dummy5;
 
 	list_for_each_entry(page, page_list, lru)
 		ClearPageActive(page);
 
 	nr_reclaimed = shrink_page_list(page_list, NULL, &sc,
 				TTU_UNMAP|TTU_IGNORE_ACCESS,
-				&dummy1, &dummy2, true);
+				&dummy1, &dummy2, &dummy3,
+				&dummy4, &dummy5, true);
 
 	while (!list_empty(page_list)) {
 		page = lru_to_page(page_list);
