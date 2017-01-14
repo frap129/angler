5857d2e12ffa05710e52d1badcd89c375196cfbe
Sultanxda <sultanxda@gmail.com>
2017-01-13 17:56:34 -0800

    msm: thermal: simple: Ensure validate_cpu_freq() only uses valid freqs
    Set the cursor to the pointer of the next valid frequency instead of
    blindly incrementing it by one for each loop iteration.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/thermal/msm_thermal_simple.c
+++ drivers/thermal/msm_thermal_simple.c
@@ -273,7 +273,7 @@ static bool validate_cpu_freq(struct cpufreq_frequency_table *pos,
 		return true;
 	}
 
-	for (;; pos++) {
+	while (1) {
 		/* This freq exists in the table so it's definitely valid */
 		if (*freq == pos->frequency)
 			return false;
@@ -291,6 +291,8 @@ static bool validate_cpu_freq(struct cpufreq_frequency_table *pos,
 			*freq = next->frequency;
 			return true;
 		}
+
+		pos = next;
 	}
 
 	return false;
