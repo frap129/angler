4aee56e7adbde544d6fff404c6822acd5da7710e
Sultanxda <sultanxda@gmail.com>
2017-06-08 12:25:42 -0700

    PM / devfreq: Remove null termination in boost_devices
    We're not passing boost_devices anywhere outside of this driver, so there's
    no need for null termination.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -46,8 +46,7 @@ static DEFINE_MUTEX(devfreq_list_lock);
 /* List of devices to boost when the screen is woken */
 static const char *boost_devices[] = {
 	"fc594000.ufshc",
-	"soc:qcom,cpubw",
-	NULL
+	"soc:qcom,cpubw"
 };
 
 #define WAKE_BOOST_DURATION_MS (5000)
@@ -1048,7 +1047,7 @@ static bool is_boost_device(struct devfreq *df)
 {
 	int i;
 
-	for (i = 0; boost_devices[i]; i++) {
+	for (i = 0; ARRAY_SIZE(boost_devices); i++) {
 		if (!strncmp(dev_name(&df->dev), boost_devices[i],
 				DEVFREQ_NAME_LEN))
 			return true;
